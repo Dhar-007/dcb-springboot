e051df88e61e8736d7579fc3be7499bc48ebffdf
Karandhar007 <Karandhar007>
2024-05-14 19:06:22 +0530

    hello controller

---
+++ src/main/java/com/dacodeb/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.dacodeb.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String Hello() {
+		return "Hello jasson";
+	}
+}
